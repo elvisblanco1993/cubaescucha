2b562d6b57f4eed9439b53d5774701b724c10b09
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2021-07-08 20:32:18 -0400

    .

--- resources/views/livewire/podcast/display-shows.blade.php
+++ resources/views/livewire/podcast/display-shows.blade.php
@@ -33,7 +33,7 @@
 
                 <a href="{{ route('podcast.display', ['podcast' => $show->url]) }}" class="col-span-2 md:col-span-1">
                     <div class="">
-                        <img src="{{ Storage::disk('s3')->url($show->thumbnail) }}" alt="{{ $show->name }}" class="rounded-lg">
+                        <img src="{{ Storage::disk('s3')->url($show->thumbnail) }}" alt="{{ $show->name }}" class="rounded-lg w-full h-48 object-cover">
                         <div class="text-xs text-blueGray-800 py-2">
                             {{ $show->name }}
                         </div>
